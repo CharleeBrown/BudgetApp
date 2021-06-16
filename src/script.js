5411bea0444cdca7b2c9a0225fa291b87067842f
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-06-16 15:20:38 -0500

    first commit

---
+++ src/script.js
@@ -0,0 +1,13 @@
+let total = document.getElementById("total");
+let rate = document.getElementById("rate");
+let hours = document.getElementById("hours");
+function totalRate(){
+    total.value =  rate.value * hours.value;
+}
+
+if( rate != null &&  hours!=null)
+{
+document.addEventListener("keyup", totalRate);
+//rate.addEventListener("change", totalRate);
+
+}
