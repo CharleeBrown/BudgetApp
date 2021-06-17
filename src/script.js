a6895db14a224bfd319df618514d0edfb93be5b7
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-06-17 11:50:22 -0500

    Can create new inputs  and new column div.

--- src/script.js
+++ src/script.js
@@ -19,3 +19,24 @@ function clearAll(){
     total.nodeValue = "";
     rate.nodeValue = "";
 }
+
+function testfunction(){
+    
+    let count = 1; 
+    let billSet = document.createTextNode("Bill #"+ count+1);
+
+    let mainRow = document.getElementById("mainCol");
+
+    let newCol = document.createElement("div")
+    
+
+    newCol.setAttribute("class","col");
+    newCol.setAttribute("style", "text-align:center;");
+
+    let newInput = document.createElement("input");
+    let text = document.createTextNode("Test");
+    
+    newCol.appendChild(newInput);
+    mainRow.appendChild(newCol);
+    
+}
